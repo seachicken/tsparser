21b01521130f683385f98223c1ae4c066c6f1a3d
Seito Tanaka <seito.tanaka.dev@gmail.com>
2022-11-13 07:19:43 +0900

    Fix converting the circular structure to JSON (#1)

--- src/tsparser.js
+++ src/tsparser.js
@@ -1,6 +1,5 @@
 import readline from 'readline';
 import ts from 'typescript';
-import './cycle';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,18 +7,34 @@ const rl = readline.createInterface({
   terminal: false
 });
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Cyclic_object_value
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
+
 rl.on('line', (input) => {
   const program = ts.createProgram([input], { allowJs: true });
   const sourceFile = program.getSourceFile(input);
 
   function parse(node) {
+    // https://github.com/microsoft/TypeScript/blob/1294ee8c1f37c8bf214db67e180edb3220a52236/src/compiler/types.ts#L21
+    node.kindName = ts.SyntaxKind[node.kind];
     node.start = node.getStart(sourceFile);
     ts.forEachChild(node, parse);
   }
   ts.forEachChild(sourceFile, parse);
 
   try {
-    process.stdout.write(`${JSON.stringify(sourceFile)}\n`);
+    process.stdout.write(`${JSON.stringify(sourceFile, getCircularReplacer())}\n`);
   } catch (error) {
     process.stdout.write(`\n`);
     process.stderr.write(`${error}\n`);
