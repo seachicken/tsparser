8b309db1a837856a711457c9a22c41ff952e93b9
seito.tanaka <seito.tanaka@linecorp.com>
2022-11-12 21:05:13 +0900

    fix

--- src/tsparser.js
+++ src/tsparser.js
@@ -1,12 +1,24 @@
 import readline from 'readline';
 import ts from 'typescript';
-import './cycle';
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
+
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
 
 rl.on('line', (input) => {
   const program = ts.createProgram([input], { allowJs: true });
@@ -19,7 +31,7 @@ rl.on('line', (input) => {
   ts.forEachChild(sourceFile, parse);
 
   try {
-    process.stdout.write(`${JSON.stringify(sourceFile)}\n`);
+    process.stdout.write(`${JSON.stringify(sourceFile, getCircularReplacer())}\n`);
   } catch (error) {
     process.stdout.write(`\n`);
     process.stderr.write(`${error}\n`);
